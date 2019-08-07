46983013725bad53c7176825f4cdb90f2a191f98
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-08-07 14:43:18 +1000

    Second Initial

--- Assets/Tests/TestSuite.cs
+++ Assets/Tests/TestSuite.cs
@@ -17,7 +17,6 @@ public class TestSuite
         // Load and spawn Game prefab
         GameObject prefab = Resources.Load<GameObject>("Prefabs/Game");
         game = Object.Instantiate(prefab);
-
         // Get GameManager
         gameManager = GameManager.Instance;
         //player = Object.FindObjectOfType<Player>();
@@ -39,6 +38,22 @@ public class TestSuite
         yield return new WaitForEndOfFrame();
 
         Assert.NotNull(player, "Where is da player?? You gotta assign da player ");
+    }
+
+    [UnityTest]
+    public IEnumerator ItemCollidesWithPlayer()
+    {
+        //Item item = gameManager.itemManager.GetItem(0);
+        //item.transform.position = player.transform.position;
+
+        GameObject itemPrefab = Resources.Load<GameObject>("Prefabs/Entities/Item");
+
+        Vector3 playerPosition = player.transform.position;
+        GameObject item = Object.Instantiate(itemPrefab, playerPosition, Quaternion.identity);        
+
+        yield return new WaitForSeconds(.1f);
+
+        Assert.IsTrue(item == null);
     }
 
     [TearDown]
