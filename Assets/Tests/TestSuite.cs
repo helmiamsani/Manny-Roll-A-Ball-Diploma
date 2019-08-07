c3c920c3616810d623f53c8f00e18de76c9b35b8
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-08-07 12:00:56 +1000

    vvv

--- Assets/Tests/TestSuite.cs
+++ Assets/Tests/TestSuite.cs
@@ -2,17 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestSuite : MonoBehaviour
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+public class TestSuite
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject game;
+    private GameManager gameManager;
+    private Player player;
+
+    [SetUp]
+    public void Setup()
     {
-        
+        // Load and spawn Game prefab
+        GameObject prefab = Resources.Load<GameObject>("Prefabs/Game");
+        game = Object.Instantiate(prefab);
+
+        // Get GameManager
+        gameManager = GameManager.Instance;
+        //player = Object.FindObjectOfType<Player>();
+        player = game.GetComponentInChildren<Player>();
     }
 
-    // Update is called once per frame
-    void Update()
+    [UnityTest]
+    public IEnumerator GamePrefabLoaded()
     {
-        
+        yield return new WaitForEndOfFrame();
+
+        // Game object should exist at this point in time
+        Assert.NotNull(game);
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerExists()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Assert.NotNull(player, "Where is da player?? You gotta assign da player ");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Object.Destroy(game);
     }
 }
